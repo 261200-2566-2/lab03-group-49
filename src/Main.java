34e08667be22ed400cf7c183918bc7034273b361
NN <rawipa_s@cmu.ac.th>
2023-11-28 01:45:26 +0700

    commented

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,33 @@
-/.../
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Player Hero1 = new Player(name: "Hero", level:1, d:20, s:10, r:3);
-        Player Hero2 = new Player(name:"Hero2", level:1, d:35, s:10, r:2);
-        Sword E = new Sword(level:5, d:50);
-        Sword B = new Sword(level:1, d:10);
-        Shield D = new Shield(level:10, s:20);
+        Player Hero1 = new Player("Hero1", 1, 20, 10, 3); //สร้างผู้เล่น Hero1
+        Player Hero2 = new Player("Hero2", 1, 35, 10, 2); //สร้างผู้เล่น Hero2
+        Sword sw1 = new Sword(5, 50); //สร้างดาบ sw1
+        Sword sw2 = new Sword(1, 10); //สร้างดาบ sw2
+        Shield sh1 = new Shield(10, 20); //สร้างโล่ sh1
 
-        Hero1.showstate();
-        Hero2.showstate();
-        Hero1.equipSword(B);
-        Hero1.showstate();
+        Hero1.showState(); //แสดงข้อมูลของ Hero1
+        Hero2.showState(); //แสดงข้อมูลของ Hero2
 
-        Hero1.attack(Hero2);
-        Hero2.showstate();
+        Hero1.equipSword(sw2); //Hero1 ถือดาบ sw2
+        Hero1.showState(); //แสดงข้อมูลของ Hero1
 
-        Hero2.equipSword(E);
-        Hero1.equipShield(D);
-        Hero1.level_up();
-        B.level_up();
-        Hero1.showstate();
-        Hero2.showstate();
+        Hero1.attack(Hero2); //Hero1 โจมตี Hero2
+        Hero2.showState(); //แสดงข้อมูลของ Hero2
 
-        Hero1.attack(Hero2);
-        Hero2.attack(Hero1);
+        Hero2.equipSword(sw1); //Hero2 ถือดาบ sw1
+        Hero1.equipShield(sh1); //Hero1 ถือโล่ sh1
+        Hero1.level_up(); //Hero1 level เพิ่มขึ้น
+        sw2.level_up(); //ดาบ sw2 level เพิ่มขึ้น
+        Hero1.showState(); //แสดงข้อมูลของ Hero1
+        Hero2.showState(); //แสดงข้อมูลของ Hero2
 
-        Hero1.showstate();
-        Hero2.showstate();
+        Hero1.attack(Hero2); //Hero1 โจมตี Hero2
+        Hero2.attack(Hero1); //Hero2 โจมตี Hero1
+        Hero1.showState(); //แสดงข้อมูลของ Hero1
+        Hero2.showState(); //แสดงข้อมูลของ Hero2
 
+        Hero2.unequipSword(); //Hero2 วางดาบ (sw1)
+        Hero2.showState(); //แสดงข้อมูลของ Hero2
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,101 +1,93 @@
+//class ของผู้เล่น
 public class Player {
     private String name;
     private int level;
-    private double max_hp,hp,max_mana,mana,d,damage,r,max_speed,s,defense;
+    private double max_hp, hp, max_mana, mana, d, damage, r, max_speed, s, defense;
     private Shield e_shield;
     private Sword e_sword;
 
-
-
+    //constructor
     public Player(String name,int level,double d, double s,double r){
-        this.name = name;
-        this.level = level;
-        this.d = d;
-        this.s = s;
-        this.r = r;
-        hp = 100+10*(level-1);
-        mana = 50+2*(level-1);
-        e_sword = null;
-        e_shield = null;
+        this.name = name; //ชื่อของผู้เล่น
+        this.level = level; //level ของผู้เล่น
+        this.d = d; //พลังโจมตีของผู้เล่น
+        this.s = s; //พลังป้องกันของผู้เล่น
+        this.r = r; //ค่าความเร็วพื้นฐานของผู้เล่น (base speed)
+        hp = 100 + 10*(level-1); //ค่าพลังชีวิตของผู้เล่น
+        mana = 50 + 2*(level-1); //ค่าพลังเวทมนตร์ของผู้เล่น
+        e_sword = null; //ไม่ถือดาบ
+        e_shield = null; //ไม่ถือโล่
     }
 
-    public void level_up() { level++;}
+    public void level_up() { level++;} //เพิ่ม level ของผู้เล่น
 
-    private void update_state(){
-        max_hp = 100+10*(level-1);
-        max_mana = 50+2*(level-1);
-        max_speed = r + (r * (0.1+0.03*(level-1)));
-        damage = d * (1 + 0.1*(level-1));
-        defense = s * (1 + 0.05*(level-1));
-        if(e_sword != null){
-            damage = damage+e_sword.getdamageSword();
-            max_speed -= (0.1+0.04*e_sword.getLevelSword());
+    private void update_state(){ //update ข้อมูลของผู้เล่น
+        max_hp = 100 + 10*(level-1); //ค่า max hp ของผู้เล่น
+        max_mana = 50 + 2*(level-1); //ค่า max mana ของผู้เล่น
+        max_speed = r + (r * (0.1 + 0.03*(level-1))); //ค่า max speed ของผู้เล่น
+        damage = d * (1 + 0.1*(level-1)); //ค่า damage (โจมตี)
+        defense = s * (1 + 0.05*(level-1)); //ค่า defense (ป้องกัน)
+        if(e_sword != null){ //ถ้าผู้เล่นถือดาบ
+            damage = damage + e_sword.getDamageSword(); //พลังโจมตีเพิ่มขึ้น
+            max_speed -= (0.1 + 0.04*e_sword.getLevelSword()); //ค่า max speed ลดลง
         }
-        if(e_shield != null){
-            defense = defense+e_shield.getdefenseShield();
-            max_speed -= (0.1+0.08*e_shield.getLevelShield());
+        if(e_shield != null){ //ถ้าผู้เล่นถือโล่
+            defense = defense + e_shield.getDefenseShield(); //พลังป้องกันเพิ่มขึ้น
+            max_speed -= (0.1 + 0.08*e_shield.getLevelShield()); //ค่า max speed ลดลง
         }
-        if(max_speed < 0 ){
-            max_speed = 0;
+        if(max_speed < 0 ){ //ถ้าค่า max speed น้อยกว่า 0
+            max_speed = 0; //set max speed = 0
         }
-
-
     }
 
-
-    public void showstate(){
-        update_state();
-        System.out.println("|---------------------------|");
-        System.out.println("    Name = " + name);
-        System.out.println("    Max Hp = " + max_hp);
-        System.out.println("    Hp = " + hp);
-        System.out.println("    Max Mana = " + max_mana);
-        System.out.println("    Mana = " + mana);
-        System.out.println("    Base Speed = " + r);
-        System.out.println("    Max Speed = " + max_speed);
-        System.out.println("    Damage = " + damage);
-        System.out.println("    Defense = " + defense);
-        if(e_sword != null){
+    public void showState(){ //บอกข้อมูลของผู้เล่น
+        update_state(); //เรียกใช้ function update_state() เพื่อ update ค่าต่างๆ แล้วแสดงค่านั้น
+        System.out.println("|------------------------------|");
+        System.out.println("    Name : " + name + " (Level " + level + ")");
+        System.out.println("    HP/Max Hp : " + hp + "/" + max_hp);
+        System.out.println("    Mana/Max Mana : " + mana + "/" + max_mana);
+        System.out.println("    Base Speed : " + r);
+        System.out.println("    Max Speed : " + max_speed);
+        System.out.println("    Damage : " + damage);
+        System.out.println("    Defense : " + defense);
+        if(e_sword != null){ //ถ้าผู้เล่นถือดาบ
             System.out.println("    equipSword");
         }
-        if(e_shield != null){
+        if(e_shield != null){ //ถ้าผู้เล่นถือโล่
             System.out.println("    equipShield");
         }
-        System.out.println("|---------------------------|");
+        System.out.println("|------------------------------|");
         System.out.println(" ");
 
     }
 
 
-    public void equipSword(Sword sword){
-        unequipSword();
+    public void equipSword(Sword sword){ //function ถือดาบ
+        unequipSword(); //เรียกใช้ function unequipSword() ก่อน (วางดาบก่อน)
         e_sword = sword;
     }
 
 
-    public void unequipSword() { e_sword = null;}
+    public void unequipSword() { e_sword = null;} //function วางดาบ
 
 
-    public void equipShield(Shield shield){
-        unequipShield();
+    public void equipShield(Shield shield){ //function ถือโล่
+        unequipShield(); //เรียกใช้ function unequipShield() ก่อน (วางโล่ก่อน)
         e_shield = shield;
     }
 
-    public void unequipShield() { e_shield = null;}
+    public void unequipShield() { e_shield = null;} //function วางโล่
 
-    public void attack(Player p) { p.Takedamage(damage);}
+    public void attack(Player p) { p.takeDamage(damage);} //function โจมตี
 
-
-    private void Takedamage(double d){
-        double dam = defense - d;
-        if(dam > 0){
-            dam = 0;
+    private void takeDamage(double d){ //function แสดงค่าพลังชีวิตของผู้เล่นหลังจากถูกโจมตี
+        double dam = defense - d; //ค่าความเสียหาย = ค่าพลังป้องกัน - ค่าพลังโจมตีที่ได้รับ
+        if(dam > 0){ //ถ้าค่าความเสียหาย > 0 (defense > d)
+            dam = 0; //ให้ค่าความเสียหายเป็น 0
         }
-        hp += dam;
-        if(hp < 0){
-            hp = 0;
+        hp += dam; //ค่าพลังชีวิตลดลง (ค่าความเสียหายต้องติดลบ เนื่องจากถูกโจมตี)
+        if(hp < 0){ //ถ้าค่าพลังชีวิต น้อยกว่า 0
+            hp = 0; //ให้ค่าพลังชีวิตเหลือ 0
         }
     }
-
-
 }
